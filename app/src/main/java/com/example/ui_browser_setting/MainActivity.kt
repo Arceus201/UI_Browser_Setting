cd89528f2c05f06df05bce014ec2ab89cd44943c
Arceus201 <hungnguyen090050@email.com>
2024-05-26 22:41:42 +0700

    Search url

--- app/src/main/java/com/example/ui_browser_setting/MainActivity.kt
+++ app/src/main/java/com/example/ui_browser_setting/MainActivity.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -15,39 +16,64 @@ class MainActivity : AppCompatActivity(), OnItemClickListener, EditUrlDialogFrag
         ActivityMainBinding.inflate(layoutInflater)
     }
     private lateinit var adapter: MyAdapter
-    private val items = mutableListOf(
-        "http://example.com/1",
-        "http://example.com/2",
-        "http://example.com/3"
-    )
+    private  var items: MutableList<UrlData>?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        items = mutableListOf()
 
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        adapter = MyAdapter(items, this, this)
+        adapter = MyAdapter(this)
         recyclerView.adapter = adapter
 
         binding.buttonAddUrl.setOnClickListener {
             addUrl()
         }
+
+        // Sự kiện khi thay đổi trên SearchView
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                filter(newText)
+                return true
+            }
+        })
     }
 
-    private fun addUrl(){
+    // Lọc danh sách URL dựa trên từ khóa từ SearchView
+    private fun filter(text: String?) {
+        val filteredItems = mutableListOf<UrlData>()
+        if (text.isNullOrBlank()) {
+            items?.let { filteredItems.addAll(it) }
+        } else {
+            val searchText = text.trim().toLowerCase()
+            items?.forEach { url ->
+                if (url.url?.toLowerCase()?.contains(searchText) == true) {
+                    filteredItems.add(url)
+                }
+            }
+        }
+        adapter.setItems(filteredItems)
+    }
+
+    private fun addUrl() {
         val newUrl = binding.editTextUrl.text.toString().trim()
         if (newUrl.isNotEmpty()) {
-            items.add(newUrl)
-            adapter.notifyItemInserted(items.size - 1)
+            items?.add(UrlData(newUrl))
+            items?.let { adapter.setItems(it) }
+            adapter.notifyItemInserted((items?.size ?: 0) - 1)
             binding.editTextUrl.text.clear()
         }
     }
 
     override fun onEditClick(position: Int) {
-        val url = items[position]
-
+        val url = items?.get(position)?.url ?: ""
         // Kiểm tra xem một DialogFragment với cùng tag đã được hiển thị hay chưa
         val existingFragment = supportFragmentManager.findFragmentByTag("EditUrlDialog")
         if (existingFragment == null) {
@@ -56,16 +82,15 @@ class MainActivity : AppCompatActivity(), OnItemClickListener, EditUrlDialogFrag
         }
     }
 
-
     override fun onDeleteClick(position: Int) {
-        val url = items[position]
+        val url = items?.get(position)?.url ?: ""
         AlertDialog.Builder(this)
             .setTitle("Bạn có chắc chắn muốn xóa?")
             .setMessage("URL: $url")
             .setPositiveButton("OK") { dialog, _ ->
-                items.removeAt(position)
+                items?.removeAt(position)
                 adapter.notifyItemRemoved(position)
-                adapter.notifyItemRangeChanged(position, items.size)
+                items?.let { adapter.notifyItemRangeChanged(position, it.size) }
                 dialog.dismiss()
             }
             .setNegativeButton("Cancel") { dialog, _ ->
@@ -91,9 +116,7 @@ class MainActivity : AppCompatActivity(), OnItemClickListener, EditUrlDialogFrag
     }
 
     override fun onDialogPositiveClick(position: Int, newUrl: String) {
-        items[position] = newUrl
+        items?.get(position)?.url = newUrl
         adapter.notifyItemChanged(position)
     }
 }
-
-
---
+++ app/src/main/java/com/example/ui_browser_setting/UrlData.kt
@@ -0,0 +1,7 @@
+package com.example.ui_browser_setting
+
+import java.net.URL
+
+data class UrlData(
+    var url: String? = ""
+)
